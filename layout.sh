bf4a61a3bdba91b9d08a7a74a69a869b882101d7
aryzhk0v <125603030+aryzhk0v@users.noreply.github.com>
2023-03-20 10:57:34 +0300

    Update scripts

--- layout.sh
+++ layout.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
+CAPS=$(xset q | grep Caps | awk '{print $4}')
 case "$(xset -q | grep -A 0 'LED' | cut -c 63)" in
-  "0") layout="US" ;;
-  "1") layout="RU" ;;
+  "0") layout="us" ;;
+  "1") layout="ru" ;;
   *) layout="unknown" ;;
 esac
-echo $layout
+[[ "$CAPS" == "on" ]] && echo "${layout^^}" || echo "${layout}"
