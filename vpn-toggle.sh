7854784be3e35621103a37ac7d9f0f156352d4bf
aryzhk0v <125603030+aryzhk0v@users.noreply.github.com>
2023-08-05 10:14:48 +0300

    Update scripts

--- vpn-toggle.sh
+++ vpn-toggle.sh
@@ -2,7 +2,7 @@
 
 STATUS="$(openvpn3 sessions-list | grep 'Client connected' | wc -l)"
 case $STATUS in
-    "0") openvpn3 session-start --config profile-8.ovpn > /dev/null 2>&1 ;;
-    "1") openvpn3 session-manage --disconnect --config profile-8.ovpn > /dev/null 2>&1 ;;
+    "0") openvpn3 session-start --config ~/profile-8.ovpn > /dev/null 2>&1 ;;
+    "1") openvpn3 session-manage --disconnect --config ~/profile-8.ovpn > /dev/null 2>&1 ;;
     *) notify-send "Error toggling VPN in ~/.scripts/vpn-toggle.sh";;
 esac
